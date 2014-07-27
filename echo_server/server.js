245529c8457ab1b868797bf5eae78a8e7986fc87
Robert Brockie <robertbrockie@gmail.com>
2014-07-27 18:59:27 -0400

    Added introduction to the echo server.

--- echo_server/server.js
+++ echo_server/server.js
@@ -2,14 +2,25 @@
 	server.js
 
 	Will create a simple echo server. Once running
-	simply connect via telnet and annoy yourself.
+	simply connect via telnet and be amazed.
 **/
 var net = require('net');
+var port = 8888;
 
 var server = net.createServer(function(socket) {
+
+	// Introduce yourself
+	socket.once ('data', function(data) {
+		socket.write('I am only going to say this once! I am a echo server!\n');
+	});
+
+	// Tell them what they said
 	socket.on('data', function(data) {
 		socket.write(data);
 	});
 });
 
-server.listen(8888);
+// Start it up
+server.listen(port, function() {
+	console.log('Echo server started on ' + server.address().address + ':' + server.address().port);
+});
